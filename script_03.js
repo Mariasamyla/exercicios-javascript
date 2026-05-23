44527a1a78b1856647a22ee0c46b1e0b5c4cfa50
Mariasamyla <maria.samyla.12@gmail>
2026-05-23 11:12:18 -0300

    exercicio3

---
+++ script_03.js
@@ -0,0 +1,18 @@
+const paragrafo = document.getElementById('minha-linha');
+const botao = document.getElementById('botao');
+const botao2 = document.getElementById('botao2');
+const botao3 = document.getElementById('botao3');
+const botao4 = document.getElementById('botao4');
+
+function seleçãoTexto{
+    if()
+
+    else();
+
+}
+
+
+
+
+botao.addEventListener('click', toggleVisibility);
+botao2.addEventListener('click', toggleVisibility);
